4a4f129e948a6379338f04c000430d0beb7d82c8
CodeSandbox <support@codesandbox.io>
2023-08-11 18:26:57 +0000

    Sandbox snapshot commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./styles.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import Home from "./components/pages/Home";
+import Projects from "./components/pages/Projects";
+import Contact from "./components/pages/Contact";
+import Company from "./components/pages/Company";
+import NewProject from "./components/pages/NewProject";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+
+import Container from "./components/layout/Container";
+
+export default function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+
+      <Footer />
+    </Router>
+  );
+}
