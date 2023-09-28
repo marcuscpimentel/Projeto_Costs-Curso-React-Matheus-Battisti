4a453e164b5a1737c9406ae7904235200d794513
CodeSandbox <support@codesandbox.io>
2023-09-28 13:13:18 +0000

    Sandbox snapshot commit

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,12 +1,37 @@
+import { useNavigate } from "react-router-dom";
 import ProjectForm from "../project/ProjectForm";
 import styles from "./NewProject.module.css";
 
 function NewProject() {
+  const navigate = useNavigate();
+
+  function CreatePost(project) {
+    //Initialize cost and services
+    project.cost = 0;
+    project.services = [];
+
+    fetch("https://hc8v3v-5000.csb.app/projects", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then(
+        (data) => console.log(data),
+        //redirect
+        navigate("/projects", {
+          message: "Projeto criado com sucesso!",
+        }),
+      )
+      .catch((err) => console.log(err));
+  }
   return (
     <div className={styles.newproject_container}>
       <h1>Criar Projeto</h1>
       <p>Crie o seu projeto para depois adicionar os serviços</p>
-      <ProjectForm btnText="Criar Projeto" />
+      <ProjectForm handleSubmit={CreatePost} btnText="Criar Projeto" />
     </div>
   );
 }
