c16f3dc307937504b9f2a8cbcb58247f63895772
CodeSandbox <support@codesandbox.io>
2023-10-31 17:55:02 +0000

    Sandbox snapshot commit

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,16 +1,35 @@
 import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import Message from "../layout/Message";
 import Container from "../layout/Container";
 import styles from "./Projects.module.css";
 import LinkButton from "../layout/LinkButton";
+import ProjectCard from "../project/ProjectCard";
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
+
   const location = useLocation();
   let message = "";
   if (location.state) {
     message = location.state.message;
   }
+
+  useEffect(() => {
+    fetch("https://hc8v3v-5000.csb.app/projects", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+        setProjects(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className={styles.project_container}>
@@ -20,7 +39,16 @@ function Projects() {
       </div>
       {message && <Message type="success" msg={message} />}
       <Container customClass="start">
-        <p>Projetos...</p>
+        {projects.length > 0 &&
+          projects.map((project) => (
+            <ProjectCard
+              name={project.name}
+              id={project.id}
+              budget={project.budget}
+              category={project?.category?.name}
+              key={project.id}
+            />
+          ))}
       </Container>
     </div>
   );
